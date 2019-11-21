2e7ab6505bf82b2b8ee2c6b06ef819c040be6c19
vagrant <hbkmustang@gmail.com>
2019-11-22 01:14:21 +0200

    COMMIT local 1

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,80 +1,17 @@
-properties([
-	buildDiscarder(
-        logRotator(
-            artifactDaysToKeepStr: '14',
-            artifactNumToKeepStr: '5',
-            daysToKeepStr: '14',
-            numToKeepStr: '10'
-        )
-    ),
-    parameters([
-        choice(
-            name: 'BRANCH',
-            choices: 'master\nstable\nrelease',
-            description: 'Choise master, stable, release'
-        )
-    ]),
-    pipelineTriggers([
-        GenericTrigger(
-            causeString: 'Push to master', 
-            genericVariables: [[
-                defaultValue: '',
-                key: 'ref', 
-                regexpFilter: '', 
-                value: '$.ref'
-            ]], 
-            printContributedVariables: true, 
-            printPostContent: true, 
-            regexpFilterExpression: 'master$', 
-            regexpFilterText: '$ref', 
-            silentResponse: true, 
-            token: '71B6B68DFC8C34235B642BE12346ifjgk'
-        )
-    ])
-])
-
-node () {
-	currentBuild.displayName = "#${BUILD_NUMBER} text1 ${params.BRANCH}"
-	ansiColor('xterm') {
-        printlnGreen "ttexttt"
-    }
-
-    stage ("average") {
-        sh"uptime"
-        deleteDir()
-    }
-    // stage () {
-    //    sshagent(['uuuuuuuuuuuuu']) {
-    //        sh "git clone -b master git@github.com:/tests.git ."
-    //    }
-    // }   
-    // input("Please approved deploy to...")
-}
-
 node {
-    stage ("gitclone") {
+    stage ("CHECKOUT") {
         // withCredentials([usernamePassword(credentialsId: '6c7686a5-6762-426c-968e-1758974df0f9', passwordVariable: 'Password', usernameVariable: 'Username')]) {
-        //    sh " git clone https://$Username:$Password@github.com/hbkmustang/spring-boot-mustang-copy"
-        //    // sh " git clone https://$Username:$Password@github.com/hbkmustang/testtesttest"
+        sh " git clone https://github.com/hbkmustang/spring-boot-mustang-copy"
         //    }
-		sh "ls -lh"
-		checkout scm
-		sh "ls -lh"
+		// checkout scm
     }
-    // stage ("workdir") {
-    //     def workDir = sh(returnStdout: true, script: "pwd").trim()
-    //     sh "cd $workDir && cd testtesttest && ls -lh"
-    // }
-    stage ("buildgw") {
+
+    stage ("BUILD") {
         sh "mvn clean install -f spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-ui/"
     }
+    
     stage ("putfileinJenkins") {
         // def workDir = sh(returnStdout: true, script: "pwd").trim()
         archiveArtifacts artifacts: "**/target/*.jar", fingerprint: true
     } 
 }
-
-def printlnGreen(text) {
-    println "\033[1;4;37;42m$text\033[0m"
-}
-
