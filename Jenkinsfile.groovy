7883a97f730883a3d8fdd129e5eea6292533fe99
vagrant <hbkmustang@gmail.com>
2019-11-24 15:33:43 +0200

    Trigger Webhook

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,12 +15,12 @@ node () {
 
 	
     stage ("BUILD") {
-        sh "pwd"
+        // sh "pwd"
         sh "mvn clean install -f spring-boot-mustang/spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-ui/"
-        sh "mv spring-boot-mustang/spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-ui/target/spring-boot-smoke-test-web-ui-2.2.1.BUILD-SNAPSHOT.jar spring-boot-mustang/spring-project.1.0.${env.BUILD_NUMBER}.jar"
-        sh "ls -lh spring-boot-mustang/"
-        def versionbuild = env.BUILD_NUMBER
-        echo versionbuild
+        // sh "mv spring-boot-mustang/spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-ui/target/spring-boot-smoke-test-web-ui-2.2.1.BUILD-SNAPSHOT.jar spring-boot-mustang/spring-project.1.0.${env.BUILD_NUMBER}.jar"
+        // sh "ls -lh spring-boot-mustang/"
+        // def versionbuild = env.BUILD_NUMBER
+        // echo versionbuild
     }
     
     // stage ("putfileinJenkins") {
@@ -30,22 +30,22 @@ node () {
 
 
     stage ("publish to nexus") {
-        echo versionbuild
-        sh "pwd"
+        // echo "Version Build: " versionbuild
+        // sh "pwd"
         nexusArtifactUploader(
             nexusVersion: 'nexus3',
             protocol: 'http',
             nexusUrl: '127.0.0.1:8081',
-            groupId: 'org.springframework.boot',
-            version: '${env.BUILD_NUMBER}',
+            groupId: 'GW',
+            version: '1.0.'+env.BUILD_NUMBER,
             repository: 'spring-repo',
             credentialsId: 'nexus-credentials',
             artifacts: [
-                [artifactId: 'spring-boot-smoke-test-web-ui',
+                [artifactId: 'spring-project',
                 classifier: '',
                 // file: 'spring-boot-mustang/spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-ui/target/spring-boot-smoke-test-web-ui-2.2.1.BUILD-SNAPSHOT.jar',
                 // file: '**/target/*.jar',
-                file: 'spring-project.1.0' + versionbuild + '.jar',
+                file: 'spring-boot-mustang/spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-ui/target/spring-boot-smoke-test-web-ui-2.2.1.BUILD-SNAPSHOT.jar',
                 type: 'jar']
             ]
         )
