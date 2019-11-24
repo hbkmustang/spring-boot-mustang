38bb601123b99f3ccfc96220314582909e54b404
vagrant <hbkmustang@gmail.com>
2019-11-24 03:50:32 +0200

    Trigger Webhook

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -31,7 +31,7 @@ node () {
             protocol: 'http',
             nexusUrl: '127.0.0.1:8081',
             groupId: 'org.springframework.boot',
-            version: 2.2.1.BUILD-SNAPSHOT,
+            version: '2.2.1.BUILD-SNAPSHOT',
             repository: 'spring-repo',
             credentialsId: 'nexus-credentials',
             artifacts: [
