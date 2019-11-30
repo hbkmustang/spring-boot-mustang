a67e0a35dd5cd37f32128076f4616bcfff437705
vagrant <hbkmustang@gmail.com>
2019-11-30 17:22:25 +0200

    new system is trying to start

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -67,8 +67,8 @@ node () {
     stage ("CI DEPLOY") {
         // build 'QA_provisioning_deploy'
         // build '../ci-Instance/jobs/create'
-        sh "echo Try"
-        build '../ci-Instance/jobs/deploy'
+        // sh "echo Try"
+        // build '../ci-Instance/jobs/deploy'
         sh "echo Try"
         build 'ci-Instance/deploy'
         // build 'ci-Instance/jobs/deploy_in_docker'
